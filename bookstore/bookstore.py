8b6880efa01cfd795974530409b67f2d56c0f1e2
Cord Graham <cord.graham@gmail.com>
2017-06-17 16:27:06 +0000

    Finalized and all testing passed

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,39 @@
 class Bookstore(object):
-    pass
-
-
+    def __init__(self, name):
+      self.name = name
+      self.books = []  
+    
+    def get_books(self):
+      return self.books
+    
+    def add_book(self, Book):
+      self.books.append(Book)
+    
+    def search_books(self, title="/( O o )/ ", author=None):
+      listofbooksbyauthor = []
+      listofbooksbytitle = []
+      for book in self.books:
+        if title.lower() in book.title.lower():
+          listofbooksbytitle.append(book)
+        elif book.author == author:
+          listofbooksbyauthor.append(book)
+      if len(listofbooksbyauthor) != 0:
+        return listofbooksbyauthor
+      else:
+        return listofbooksbytitle
+    
 class Author(object):
-    pass
-
-
+    def __init__(self, name, nationality):
+      self.name = name
+      self.nationality = nationality
+      self.books = []
+  
+    def get_books(self):
+      return self.books
+      
 class Book(object):
-    pass
+    
+    def __init__(self, title, author):
+      self.title = title
+      self.author = author
+      author.books.append(self)
